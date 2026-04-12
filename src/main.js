c8057c89d7e2c9f5f03c1c3d5609a7e4457ea271
confusedbuffalo <236869084+confusedbuffalo@users.noreply.github.com>
2026-04-12 19:11:11 +0200

    Fix meta info

--- src/main.js
+++ src/main.js
@@ -211,11 +211,11 @@ async function* createOsmElementStream(filePath) {
             tags: item.tags || {},
             lat: item.lat,
             lon: item.lon,
-            timestamp: item.timestamp,
-            version: item.version,
-            changeset: item.changeset,
-            user: item.user,
-            uid: item.uid
+            timestamp: item.info.timestamp,
+            version: item.info.version,
+            changeset: item.info.changeset,
+            user: item.info.user,
+            uid: item.info.uid
         };
     }
 }
