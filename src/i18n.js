5b5dba9e403b57eed8e13ad93b1d79042d4bfe01
confusedbuffalo <236869084+confusedbuffalo@users.noreply.github.com>
2026-05-16 20:41:19 +0200

    Fix placeholders

--- src/i18n.js
+++ src/i18n.js
@@ -18,12 +18,15 @@ const DEFAULT_LOCALE = 'en';
 const KEY_PLACEHOLDERS = {
     'invalidNumbersOutOf': ['%i', '%f', '%t'],
     'incompleteNamesOutOf': ['%i', '%t'],
+    'invalidHoursOutOf': ['%i', '%f', '%t'],
     'invalidPercentageOfTotal': ['%p'],
     'fixablePercentageOfInvalid': ['%p'],
     'reportSubtitleForCountry': ['%c'],
     'reportSubtitleNamesForCountry': ['%c'],
+    'reportSubtitleHoursForCountry': ['%c'],
     'countryReportTitle': ['%c'],
     'countryReportTitleNames': ['%c'],
+    'countryReportTitleHours': ['%c'],
     'editIn': ['%e'],
     'numberDetailsNamesDataFrom': ['%o'],
     'dataSourcedTemplate': ['%d', '%t', '%z', '%a']
