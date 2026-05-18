3c64b2a2f59d1b76d1be7fc97c045fe1c51fac54
confusedbuffalo <236869084+confusedbuffalo@users.noreply.github.com>
2026-05-18 12:10:37 +0200

    Test run

--- src/main.js
+++ src/main.js
@@ -455,9 +455,9 @@ async function processCountry(countryData) {
                     countryData.timestamp = dataTimestamp;
                 }
 
-                if (testMode) {
-                    break;
-                }
+                // if (testMode) {
+                //     break;
+                // }
             }
         }
     }
